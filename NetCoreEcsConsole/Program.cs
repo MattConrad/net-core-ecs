2e375cbe1a5474ef16ba32098cc2c7839248879c
Matt Conrad <mattconrad@gmail.com>
2017-05-02 19:45:27 -0500

    Start on input handling.

--- NetCoreEcsConsole/Program.cs
+++ NetCoreEcsConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using SampleGame;
 
 namespace NetCoreEcsConsole
@@ -8,6 +9,11 @@ namespace NetCoreEcsConsole
         static void Main(string[] args)
         {
             var game = new Game();
+            var heroActionDict = Switchboard.HerosActions();
+            var actionInputSet = GetKeysToActions(heroActionDict);
+            //this might not even happen until the game has started and presented the player with an initial scenario.
+            var nextHeroAction = GetHeroAction(actionInputSet);
+            Console.WriteLine(nextHeroAction);
 
             var lines = game.ProcessInput("nothing");
             foreach (string line in lines)
@@ -15,5 +21,48 @@ namespace NetCoreEcsConsole
                 Console.WriteLine(line);
             }
         }
+
+        static Dictionary<char, TextActionPair> GetKeysToActions(Dictionary<string, string> heroActionDict)
+        {
+            Dictionary<char, TextActionPair> keyToTAP = new Dictionary<char, TextActionPair>();
+
+            int i = 49;
+            foreach (string action in heroActionDict.Keys)
+            {
+                string text = heroActionDict[action];
+                char letter = Convert.ToChar(i);
+                i++;
+
+                keyToTAP.Add(letter, new TextActionPair { Text = text, Action = action });
+
+                if (i == 58) i = 65;
+                if (i > 90) throw new InvalidOperationException("You were supposed to rewrite this long ago.");
+            }
+
+            return keyToTAP;
+        }
+
+        static string GetHeroAction(Dictionary<char, TextActionPair> keyToTextActionPair)
+        {
+            Console.WriteLine("Choose an action:");
+            foreach(char key in keyToTextActionPair.Keys)
+            {
+                Console.WriteLine($"{key}: {keyToTextActionPair[key].Text}");
+            }
+
+            ConsoleKeyInfo cki;
+            while (!keyToTextActionPair.ContainsKey(cki.KeyChar))
+            {
+                cki = Console.ReadKey();
+            }
+
+            return keyToTextActionPair[cki.KeyChar].Action;
+        }
+    }
+
+    public class TextActionPair
+    {
+        public string Text { get; set; }
+        public string Action { get; set; }
     }
 }
---
+++ SampleGame/Switchboard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleGame
+{
+    public static class Switchboard
+    {
+        //eventually, this should be contextual for the hero at any given point in time.
+        //  well, the hero and any npcs that the player might be controlling
+        //  also, it would be nice if these were nested, so that Change Stance would 
+        //  subordinate the different stances.
+        public static Dictionary<string, string> HerosActions()
+        {
+            return new Dictionary<string, string>
+            {
+                ["Attack (Melee)"] = "attack-melee",                  ["Stance (Defensive)"] = "stance-defensive",                  ["Stance (Stand Ground)"] = "stance-stand-ground",  
+                ["Stance (Aggressive)"] = "stance-aggressive"
+            };
+        }
+    }
+}
