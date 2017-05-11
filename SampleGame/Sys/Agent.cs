23d1ee4ef0bb09c988ca10196bc394dee13d88ea
Matt Conrad <mattconrad@gmail.com>
2017-05-10 21:35:02 -0500

    And now it doesn't even build.

--- SampleGame/Sys/Agent.cs
+++ SampleGame/Sys/Agent.cs
@@ -1,15 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 using EntropyEcsCore;
 
 namespace SampleGame.Sys
 {
     internal static class Agent
     {
-        internal static void ExecuteCombatAction(EcsRegistrar rgs, string iDontKnowWhatImDoing)
+        internal static string GetCombatAction(EcsRegistrar rgs, string attackerAI, long attackerId, List<long> battlefieldEntityIds)
         {
+            if (attackerAI == Parts.Agent.Vals.AI.MeleeOnly) return CombatActionMeleeOnly(rgs, attackerId, battlefieldEntityIds);
 
+            throw new ArgumentException($"Attacker AI {attackerAI} not supported.");
+        }
+
+        private static string CombatActionMeleeOnly(EcsRegistrar rgs, long attackerId, List<long> battlefieldEntityIds)
+        {
+            string action = "do-nothing";
+            var factionDeltas = new Dictionary<long, int>();
+
+            var attackerFaction = rgs.GetPartsOfType<Parts.Faction>(attackerId).SingleOrDefault();
+
+            //this will do for now, but a) faction shouldn't be as important as perceived danger level, b) this is busier than maybe it should be, and c) can you really perceive faction by looking at someone?
+            // i am not sure I want numeric factions at all any more. seemed tidy--but are they really useful?
+            if (attackerFaction != null)
+            {
+                foreach(long entityId in battlefieldEntityIds)
+                {
+                    if (entityId == attackerId) continue;
+
+                    var entityFactionReputations = rgs.GetPartsOfType<Parts.Faction>(entityId).SingleOrDefault()?.FactionReputations;
+                    if (entityFactionReputations == null) continue;
+
+                    int delta = attackerFaction
+                        .FactionReputations
+                        .Select(kvp => (entityFactionReputations.ContainsKey(kvp.Key) ? kvp.Value - entityFactionReputations[kvp.Key] : 0))
+                        .Sum(d => Math.Abs(d));
+
+                    factionDeltas.Add(entityId, delta);
+                }
+            }
         }
     }
 }
--- SampleGame/Sys/Battlefield.cs
+++ SampleGame/Sys/Battlefield.cs
@@ -8,20 +8,37 @@ namespace SampleGame.Sys
 {
     internal static class Battlefield
     {
-        internal static IEnumerable<List<string>> RunBattlefield(EcsRegistrar rgs, long battlefieldId)
+        internal static IEnumerable<List<string>> RunBattlefield(EcsRegistrar rgs, long battlefieldId, Func<string> receivePlayerInput)
         {
             while (true)
             {
+                //eventually, entities may enter or leave the battlefield, so we requery every iteration.
                 var battlefieldEntityIds = rgs.GetPartsOfType<Parts.Container>(battlefieldId)
                     .Single(p => p.Tag == Parts.Container.Vals.Tag.Battlefield)
-                    .EntityIds;
+                    .EntityIds
+                    .ToList();
 
-                foreach(long entityId in battlefieldEntityIds)
+                foreach(var id in battlefieldEntityIds)
                 {
+                    var battlefieldAgent = rgs.GetPartsOfType<Parts.Agent>(id).SingleOrDefault();
+                    if (battlefieldAgent == null) continue;
+
+                    string nextActionSet;
+                    if (battlefieldAgent.ActiveCombatAI == Parts.Agent.Vals.AI.PlayerChoice)
+                    {
+                        //MWCTODO: this step should also include piping the current permitted actions out to the player.
+                        // for now these are fixed, but later they'll vary with context (drop your weapon and you're punching not slashing).
+                        nextActionSet = receivePlayerInput();
+                    }
+                    else
+                    {
+                        nextActionSet = Sys.Agent.GetCombatAction(battlefieldAgent.ActiveCombatAI, id, battlefieldEntityIds);
+                    }
+
 
                 }
 
-                //eventually, entities may enter or leave the battlefield, so let's recheck every time.
+
 
                 //var results = GettingStuff();
 
